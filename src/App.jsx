49b7a57101a830c47bf7618d9fc06a14dd783897
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-03-04 20:17:42 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Header from './assets/components/Header'
-import BmiForm from './assets/components/BmiForm'
+import Header from './components/Header'
+import BmiForm from './components/BmiForm'
 
 function App() {
   
