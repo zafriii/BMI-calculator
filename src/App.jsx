cc5f521c296d861cc80f48df0e6834edf2532afa
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-03-04 20:01:53 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './assets/components/Header'
 import BmiForm from './assets/components/BmiForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
 
     <>
